52b078c5f054664cf209bc489a7875b133b564e5
Yuhui Li <a@yuhuili.com>
2017-04-25 22:13:50 -0700

    Add partial 07-modules

---
+++ CornellCS3110/07-modules/dict.ml
@@ -0,0 +1,25 @@
+module type Dictionary = sig
+  type ('k, 'v) t
+
+  (* The empty dictionary *)
+  val empty  : ('k, 'v) t
+
+  (* [insert k v d] produces a new dictionary [d'] with the same mappings
+   * as [d] and also a mapping from [k] to [v].  If [k] was already mapped
+   * in [d], that mapping is replaced in [d'] with the new mpaping. *)
+  val insert : 'k -> 'v -> ('k,'v) t -> ('k,'v) t
+
+  (* [lookup k d] returns the value associated with [k] in [d].
+   * raises:  [Not_found] if [k] is not mapped to any value in [d]. *)
+  val lookup  : 'k -> ('k,'v) t -> 'v
+end
+
+module AssocListDict = struct
+  type ('k, 'v) t = ('k * 'v) list
+
+  let empty = []
+
+  let insert k v d = (k * v) :: d
+
+  let lookup k d = List.assoc k d
+end
---
+++ CornellCS3110/07-modules/print_int_list.ml
@@ -0,0 +1,6 @@
+let rec print_int_list = function
+  | [] -> ()
+  | h::t -> print_endline (string_of_int h); print_int_list t
+
+let print_int_list' lst =
+  List.iter (fun x -> print_endline (string_of_int x)) lst
